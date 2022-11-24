24f543c757949ee3f5977298e1800bbaf67f2b6e
P00JARY <ashilpoojari2212@gmail.com>
2022-11-24 12:25:51 +0530

    final commit

--- app/src/config/db.config.ts
+++ app/src/config/db.config.ts
@@ -1,6 +1,14 @@
 
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// export const dbconfig: TypeOrmModuleOptions = {
+export const dbconfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    port: parseInt(<string>process.env.POSTGRES_PORT),
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    synchronize: false,
 
-//     entities: ["dist/**/*.entity{.ts,.js}"],
-// }
+    entities: ["dist/**/*.entity{.ts,.js}"],
+}
